31a261474664d9d107937b0450f2356118ebf85b
Khdscor <khds_cor@naver.com>
2024-02-28 21:44:04 +0900

    초기 프로젝트 생성

---
+++ e-commerce/src/main/java/khds/ecommerce/ECommerceApplication.java
@@ -0,0 +1,13 @@
+package khds.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApplication.class, args);
+	}
+
+}
