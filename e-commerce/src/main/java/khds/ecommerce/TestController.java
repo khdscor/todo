0144a6725074aa553c8be75c505ae4459c6dd176
Khdscor <khds_cor@naver.com>
2024-04-06 10:34:04 +0900

    feat: RestController 테스트

--- e-commerce/src/main/java/khds/ecommerce/TestController.java
+++ e-commerce/src/main/java/khds/ecommerce/TestController.java
@@ -1,19 +1,18 @@
 package khds.ecommerce;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TestController {
 
     @GetMapping("/index")
     public String test(){
-        return "/index";
+        return "dsasdindex";
     }
 
     @GetMapping("/products")
     public String test2(){
-        return "/products/products";
+        return "testsfdfs";
     }
 }
