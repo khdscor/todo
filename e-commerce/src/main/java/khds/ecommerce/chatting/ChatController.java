e1e169ef304eb2e70f2e6bc932169e23e05621d3
Khdscor <khds_cor@naver.com>
2024-04-09 23:40:19 +0900

    feat: 웹소켓을 통한 실시간 채팅 서버 실습

---
+++ e-commerce/src/main/java/khds/ecommerce/chatting/ChatController.java
@@ -0,0 +1,29 @@
+package khds.ecommerce.chatting;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/chat")
+public class ChatController {
+
+    private final ChatService service;
+
+    @PostMapping
+    public ChatRoom createRoom(@RequestParam String name) {
+        return service.createRoom(name);
+    }
+
+    @GetMapping
+    public List<ChatRoom> findAllRooms() {
+        return service.findAllRoom();
+    }
+}
