7aed1fc0ee29a30d44fe2bf71731458e18e9fa0e
khdscor <khds_cor@naver.com>
2024-05-01 22:30:04 +0900

    feat: 비동기적으로 리스트 조회 시(다른 케이스 추가)

--- e-commerce/src/main/java/khds/ecommerce/chatting/Test2Controller.java
+++ e-commerce/src/main/java/khds/ecommerce/chatting/Test2Controller.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -45,7 +46,7 @@ public class Test2Controller {
         return "success";
     }
 
-    @GetMapping("/saveR2")
+    @PostMapping("/saveR2")
     public Mono<ChattingContent> testSave2(@RequestParam("name") String name, @RequestParam("age") Long age) {
         ChattingContent content = new ChattingContent(name, age);
 
@@ -74,27 +75,37 @@ public class Test2Controller {
         return "success";
     }
 
-    @GetMapping("/saveR5")
-    public Mono<ResponseEntity<ChattingContent>> testSave5(@RequestParam("name") String name, @RequestParam("age") Long age) {
-
-        ChattingContent content = new ChattingContent(name, age);
-        Mono<ChattingContent> test = test3Repository.save(content);
-        return test.map(ResponseEntity::ok);
-    }
-
-    @GetMapping("/saveR6")
+    @PostMapping("/saveR6")
     public Mono<ResponseEntity<Void>> testSave6(@RequestParam("name") String name, @RequestParam("age") Long age) {
 
         ChattingContent content = new ChattingContent(name, age);
 
-        return test3Repository.save(content).then(Mono.just(new ResponseEntity<Void>(HttpStatus.CREATED)));
+        return test3Repository.save(content).map(c -> ResponseEntity.status(HttpStatus.CREATED).build());
     }
 
     @GetMapping("/findR1")
     public Mono<ResponseEntity<List<ChattingContent>>> testFind1() {
 
-        return test3Repository.findAll().collectList().map(ResponseEntity::ok)
+        return test3Repository.findChattingContentByName("khds").collectList().map(ResponseEntity::ok)
                 .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
+    @GetMapping("/findR2")
+    public Mono<ResponseEntity<ChattingContent>> testFind2(@RequestParam("id") String id) {
+        Mono<ChattingContent> test = test3Repository.findById(id);
+        return test.map(ResponseEntity::ok).defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/findR3")
+    public Flux<ResponseEntity<ChattingContent>> testFind3(@RequestParam("name") String name) {
+
+        return test3Repository.findChattingContentByName(name).map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/findR4")
+    public Flux<ChattingContent> testFind4(@RequestParam("name") String name) {
+
+        return test3Repository.findChattingContentByName(name);
+    }
 }
