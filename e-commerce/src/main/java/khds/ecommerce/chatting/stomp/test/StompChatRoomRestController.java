f7d238e48f3db99844a90c36e9c6559a5cc2776f
Khdscor <khds_cor@naver.com>
2024-05-13 16:47:17 +0900

    feat: 프론트 리스트로 표현

--- e-commerce/src/main/java/khds/ecommerce/chatting/stomp/test/StompChatRoomRestController.java
+++ e-commerce/src/main/java/khds/ecommerce/chatting/stomp/test/StompChatRoomRestController.java
@@ -1,15 +1,10 @@
 package khds.ecommerce.chatting.stomp.test;
 
 import java.util.List;
-import khds.ecommerce.chatting.stomp.StompChatDTO;
-import khds.ecommerce.chatting.stomp.StompChatService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,13 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class StompChatRoomRestController {
 
-//    @Autowired
-//    private StompChatService chatService;
-
     private final SimpMessageSendingOperations template;
 
     // 채팅 리스트 화면
-    // / 로 요청이 들어오면 전체 채팅룸 리스트를 담아서 return
     @GetMapping("/chat/{id}")
     public ResponseEntity<List<ChatMessage>> getChatMessages(@PathVariable Long id){
         ChatMessage test = new ChatMessage(1L, "test", "test");
@@ -35,13 +26,11 @@ public class StompChatRoomRestController {
         return ResponseEntity.ok().body(List.of(test));
     }
 
+    //메세지 송신 및 수신
     @MessageMapping("/message")
-    public ResponseEntity<String> receiveMessage(@RequestBody ChatMessage chat) {
-        // 메시지 저장
-        System.out.println("chat = " + chat);
-
+    public ResponseEntity<Void> receiveMessage(@RequestBody ChatMessage chat) {
         // 메시지를 해당 채팅방 구독자들에게 전송
         template.convertAndSend("/sub/chatroom/1", chat);
-        return ResponseEntity.ok("메시지 전송 완료");
+        return ResponseEntity.ok().build();
     }
 }
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,74 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
 import axios from "axios";
 import React, { useEffect, useState, useRef } from "react";
-import { useParams } from "react-router-dom";
 import { Stomp } from "@stomp/stompjs";
 
 function App() {
    
   const stompClient = useRef(null);
-  const [messages, setMessages] = new useState();
-  const [message, setMessage] = useState("");
+  // 채팅 내용들을 저장할 변수
+  const [messages, setMessages] = new useState([]);
+   // 사용자 입력을 저장할 변수
+   const [inputValue, setInputValue] = useState('');
+   // 입력 필드에 변화가 있을 때마다 inputValue를 업데이트
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
 
-  // 웹소켓 연결 설정
+   // 웹소켓 연결 설정
   const connect = () => {
     const socket = new WebSocket("ws://localhost:8080/ws-stomp");
     stompClient.current = Stomp.over(socket);
     stompClient.current.connect({}, () => {
-      stompClient.current.subscribe(`/sub/chatroom/1`, (message) => {
-        console.log("성공!")
-         const newMessage = JSON.parse(message.body);
-        setMessages((prevMessages) => [...prevMessages, newMessage]);
-
-        console.log(newMessage.senderSeq)
-        // if (newMessage.senderSeq !== currentUser.userSeq) {
-        //   // setCustomerSeq(newMessage.senderSeq);
-        // }
+    stompClient.current.subscribe(`/sub/chatroom/1`, (message) => {
+    const newMessage = JSON.parse(message.body);
+    setMessages((prevMessages) => [...prevMessages, newMessage]);
+    // if (newMessage.senderSeq !== currentUser.userSeq) {
+    //   // setCustomerSeq(newMessage.senderSeq);
+    // }
       });
     });
     console.log("방 번호", 1);
   };
   // 웹소켓 연결 해제
-  // const disconnect = () => {
-  //   if (stompClient.current) {
-  //     stompClient.current.disconnect();
-  //   }
-  // };
+  const disconnect = () => {
+    if (stompClient.current) {
+      stompClient.current.disconnect();
+    }
+  };
   // 기존 채팅 메시지를 서버로부터 가져오는 함수
   const fetchMessages = () => {
     return axios.get("http://localhost:8080/chat/1" )
            .then(response => {setMessages(response.data)});
     
   };
-
    useEffect(() => {
     connect();
     fetchMessages();
-    // // 컴포넌트 언마운트 시 웹소켓 연결 해제
-    // return () => disconnect();
+    // 컴포넌트 언마운트 시 웹소켓 연결 해제
+    return () => disconnect();
   }, []);
 
+  //메세지 전송
   const sendMessage = () => {
-    if (stompClient.current) {
+    if (stompClient.current && inputValue) {
       const body = {
         id : 1,
         name : "테스트1",
-        message : "테스트입니다."
+        message : inputValue
       };
       stompClient.current.send(`/pub/message`, {}, JSON.stringify(body));
-      console.log("11111111111111111")
-      setMessage(""); // 입력 필드 초기화
+      setInputValue('');
     }
   };
 
-
-
-  console.log(messages)
   return (
     <div>
-      테스트입니다.!
-      <button onClick={()=>{sendMessage()}}>버튼</button>
+      {/* 리스트 출력 */}
+      <ul>
+        <div>
+          {/* 입력 필드 */}
+       <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      {/* 추가 버튼 */}
+      <button onClick={sendMessage}>입력</button>
+        </div>
+        {messages.map((item, index) => (
+          <div key={index} className="list-item">{item.message}</div>
+        ))}
+      </ul>
     </div>
   );
 }
