d748e9fa43c18f472fb1a6fc2df41634528c4e85
Khdscor <khds_cor@naver.com>
2024-05-25 21:42:18 +0900

    feat: ControllerAdvice를 통한 404 예외 처리

--- e-commerce/src/main/java/khds/ecommerce/exception/ControllerAdvice.java
+++ e-commerce/src/main/java/khds/ecommerce/exception/ControllerAdvice.java
@@ -1,18 +1,18 @@
 package khds.ecommerce.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
-@RestControllerAdvice
+@org.springframework.web.bind.annotation.ControllerAdvice
 public class ControllerAdvice {
 
     @ExceptionHandler(NoHandlerFoundException.class)
-    public ResponseEntity<ErrorResponse> handleNotIncludedMapException(NoHandlerFoundException e) {
-
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-            .body(new ErrorResponse("잘못된 URL입니다. 다시 한번 확인해 주세요 "));
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleNotFoundError(NoHandlerFoundException ex, Model model) {
+        model.addAttribute("errorMessage", "Sorry, the page you are looking for does not exist.");
+        return "error/404"; // error/404.html 파일로 이동
     }
 }
