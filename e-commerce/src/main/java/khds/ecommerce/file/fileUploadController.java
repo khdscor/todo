8985253a4b476ad0b0767c109c21b23c1b30b18b
Khdscor <khds_cor@naver.com>
2024-05-27 16:17:28 +0900

    feat: 백엔드 파일 업로드 구현

--- e-commerce/src/main/java/khds/ecommerce/file/fileUploadController.java
+++ e-commerce/src/main/java/khds/ecommerce/file/fileUploadController.java
@@ -1,15 +1,46 @@
 package khds.ecommerce.file;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import jakarta.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@RequiredArgsConstructor
 public class fileUploadController {
 
-    @GetMapping("/file/upload")
-    public ResponseEntity<Void> upload(){
+    final String filepath = "C:/Users/khdsc/IdeaProjects/e-commerce/e-commerce/src/main/resources/static/images/";
 
-        return ResponseEntity.ok().build();
+    private final FileRepository fileRepository;
+
+    @PostMapping("image")
+    public FileEntity uploadImage(HttpServletRequest request,
+        @RequestParam(value="file", required = false) MultipartFile[] files,
+        @RequestParam(value="comment", required = false) String comment) {
+
+        String FileNames = "";
+
+        String originFileName = files[0].getOriginalFilename();
+        long fileSize = files[0].getSize();
+        String safeFile = System.currentTimeMillis() + originFileName;
+
+        File f1 = new File(filepath + safeFile);
+        try {
+            files[0].transferTo(f1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        final FileEntity file = FileEntity.builder()
+            .filename(safeFile)
+            .newDate(new Date())
+            .build();
+
+        return fileRepository.save(file);
     }
 }
